22a8deadc6c5ec270c73ae9c166949f778fde813
ChenYingYu <chenalan@ChenAlandeMacBook-Air.local>
2018-01-04 15:16:00 +0800

    Change theme color

--- redo_Concentration/ViewController.swift
+++ redo_Concentration/ViewController.swift
@@ -71,14 +71,14 @@ class ViewController: UIViewController
                           "cars": #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1),
                           "halloween": #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1),
                           "faces": #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1),
-                          "animals": #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1),
+                          "animals": #colorLiteral(red: 0.5058823824, green: 0.3372549117, blue: 0.06666667014, alpha: 1),
                           "weather": #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)]
         let themeColorOfCard = ["sports": #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1),
                                 "cars": #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1),
                                 "halloween": #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1),
                                 "faces": #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1),
-                                "animals": #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1),
-                                "weather": #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)]
+                                "animals": #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1),
+                                "weather": #colorLiteral(red: 0.9818583131, green: 0.9282233715, blue: 1, alpha: 1)]
         
         let themeKeys = Array(theme.keys)
         let themeIndex = Int(arc4random_uniform(UInt32(themeKeys.count)))
